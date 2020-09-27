a34e5fe0e8c9290bcc2e44add355aca804f0e19c
Ren <renato_s_silva@hotmail.com>
2020-09-27 13:13:45 +1000

    car is driving in a lane

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -97,6 +98,135 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+
+          int lane = 1;
+          double ref_speed = 49.5; // mph
+          int prev_size = previous_path_x.size();
+
+
+          // points to be added
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          // reference points
+          // this is where the spline is going to start from
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          // create two intial points for the pline
+
+          if (prev_size < 2) {
+
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            // ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            // ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+
+          } else {
+
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+
+            double ref_x_prev  = previous_path_x[prev_size - 2];
+            double ref_y_prev  = previous_path_y[prev_size - 2];
+
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+
+          }
+
+          // adding three sparse points
+          for (int i = 30; i <= 90; i += 30) {
+            vector<double> next_wp = getXY(car_s+i,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            ptsx.push_back(next_wp[0]);
+            ptsy.push_back(next_wp[1]);
+          }
+
+          // shift the reference back to ref_x, ref_y
+
+          for (int i = 0; i < ptsx.size(); i++ )
+          {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = (shift_x * cos(0-ref_yaw)- shift_y*sin(0-ref_yaw));
+
+            ptsy[i] = (shift_x * sin(0-ref_yaw)+ shift_y*cos(0-ref_yaw));
+            
+          }
+
+          // create spline for these points
+          tk::spline s;
+
+          s.set_points(ptsx,ptsy);
+
+          // vector<double> next_x_vals;
+          // vector<double> next_y_vals;
+
+          // start building from the unused points from the previous path
+
+          for (int i = 0; i < previous_path_x.size(); i++) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // calculate how to break spline points to travel at desired speed
+
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt( target_x * target_x + target_y * target_y);
+
+          double x_add_on = 0.0;
+
+          for (int i = 1; i <= 50-previous_path_x.size(); i++) {
+
+            double N = target_dist / (.02*ref_speed/2.24);
+            double x_point = x_add_on + (target_x)/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+
+
+          }
+
+
+          vector<double> next_x_vals_2;
+          vector<double> next_y_vals_2;
+
+          double dist_inc = 0.4;
+          for (int i = 0; i < 50; ++i) {
+            double next_s = car_s+(i+1)*dist_inc;
+            double next_d = lane * 4 + 2;
+
+            vector<double> xy = getXY(next_s,next_d,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+            next_x_vals_2.push_back(xy[0]);
+            next_y_vals_2.push_back(xy[1]);
+          }
 
 
           msgJson["next_x"] = next_x_vals;
